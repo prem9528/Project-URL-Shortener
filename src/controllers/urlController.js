6dca1074ab68532faed0765aea9dd578fc8c802e
Surajxxx <suraj.dubey191@gmail.com>
2022-04-05 17:07:12 +0530

    a little modification done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,113 +1,131 @@
-const UrlModel = require('../models/urlModel')
-const ShortId = require('shortid') //nanoid
+const UrlModel = require("../models/urlModel");
+const ShortId = require("shortid"); //nanoid or UUID
 
+//********************************************VALIDATION FUNCTION********************************************************** */
 
-const isValid = function(value){
-    if(typeof (value) == 'undefined' || value == null) return false
-    if(typeof (value) == 'string' && value.trim().length > 0) return true    
-}
+const isValid = function (value) {
+  if (typeof value == "undefined" || value == null) return false;
+  if (typeof value == "string" && value.trim().length > 0) return true;
+  return false;
+};
 
-const isValidRequest = function(object){
-    return Object.keys(object).length > 0
-}
+const isValidRequest = function (object) {
+  return Object.keys(object).length > 0;
+};
 
-const isValidUrl = function(value){
-    let regexForUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-    return regexForUrl.test(value)
-}
+const isValidUrl = function (value) {
+  let regexForUrl =
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+  return regexForUrl.test(value);
+};
 
-const urlShortener = async function(req, res){
+//*************************************URL SHORTENER************************************************ */
 
-    const requestBody = req.body
-    const queryParams = req.query
-   
+const urlShortener = async function (req, res) {
+  const requestBody = req.body;
+  const queryParams = req.query;
 
-    if(isValidRequest(queryParams)){
-    return res.status(400).send({status : false, message : "invalid request"})    
+  if (isValidRequest(queryParams)) {
+    return res.status(400).send({ status: false, message: "invalid request" });
+  }
+
+  if (!isValidRequest(requestBody)) {
+    return res.status(400).send({ status: false, message: "data is required" });
+  }
+
+  const longUrl = req.body.longUrl;
+  const base = "http://localhost:3000";
+
+  if (!isValid(longUrl)) {
+    return res.status(400).send({ status: false, message: "URL is required" });
+  }
+
+  if (Object.keys(requestBody).length > 1) {
+    return res.status(400).send({ status: false, message: "invalid request" });
+  }
+
+  if (!isValidUrl(longUrl)) {
+    return res
+      .status(400)
+      .send({ status: false, message: "Enter a valid URL" });
+  }
+
+  try {
+    let urlCode = ShortId.generate();
+
+    const url = await UrlModel.findOne({ longUrl }).select({shortUrl : 1, longUrl : 1, urlCode : 1, _id : 0});
+
+    if (url) {
+     return res
+        .status(200)
+        .send({ status: true, message: "url shorten successfully", data: url });
+
+    } else {
+      const shortUrl = `${base}/${urlCode}`;
+
+      const urlData = {
+        urlCode: urlCode,
+        longUrl: longUrl.trim(),
+        shortUrl: shortUrl,
+      };
+
+      const newUrl = await UrlModel.create(urlData);
+
+      return res
+        .status(201)
+        .send({
+          status: true,
+          message: "url shorten successfully",
+          data: urlData,
+        });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+//****************************************GET URL****************************************************** */
+
+const getUrl = async function (req, res) {
+  const requestBody = req.body;
+  const queryParams = req.query;
+
+  try {
+    if (isValidRequest(queryParams)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "invalid request" });
     }
 
-    if(!isValidRequest(requestBody)){
-    return res.status(400).send({status : false, message : "data is required"})    
+    if (isValidRequest(requestBody)) {
+      return res
+        .status(400)
+        .send({ status: false, message: " input data is not required" });
     }
 
-    const longUrl = req.body.longUrl
-    const base = "http://localhost:3000"
+    const urlCode = req.params.urlCode;
 
-    
-  
-    if(!isValid(longUrl)){
-    return res.status(400).send({status : false, message : "URL is required"}) 
+    if (!urlCode) {
+      return res
+        .status(400)
+        .send({ status: false, message: " urlCode is required" });
     }
 
-    if(Object.keys(requestBody).length > 1){
-    return res.status(400).send({status : false, message : "invalid request"})    
+    const findUrlDetailsByCode = await UrlModel.findOne({ urlCode: urlCode.toLowerCase() });
+
+    if (!findUrlDetailsByCode) {
+      return res
+        .status(404)
+        .send({ status: false, message: "no such url exist" });
     }
 
-    if(!isValidUrl(longUrl)){
-    return res.status(400).send({status : false, message : "Enter a valid URL"})   
-    }
+    res.redirect(findUrlDetailsByCode.longUrl);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
 
-   
-   let urlCode = ShortId.generate()
+//**********************************EXPORTING BOTH HANDLERS************************************************/
 
-    try{
-        const URL = await UrlModel.findOne({longUrl})
-
-        if(URL){
-            res.status(201).send({status : true, message : "url shorten successfully", data : URL })
-
-        }else{
-            const shortUrl = `${base}/${urlCode}`  
-
-            const urlData = {
-                urlCode : urlCode,
-                longUrl : longUrl.trim(),
-                shortUrl : shortUrl
-            }
-
-            const newUrl = await UrlModel.create(urlData)
-
-            res.status(201).send({status : true, message : "url shorten successfully", data : newUrl })
-        }
-
-    }catch(err){
-        res.status(500).send({error : err.message})
-    }
-}
-
-
-const getUrl = async function(req, res){
-
-    const requestBody = req.body
-    const queryParams = req.query
-   
-try{
-    if(isValidRequest(queryParams)){
-    return res.status(400).send({status : false, message : "invalid request"})    
-    }
-
-    if(isValidRequest(requestBody)){
-    return res.status(400).send({status : false, message : " input data is not required"})    
-    }
-
-    const urlCode = req.params.urlCode
-
-    if(!urlCode){
-    return res.status(400).send({status : false, message : " urlCode is required"}) 
-    }
-
-    const findUrlDetailsByCode = await UrlModel.findOne({urlCode : urlCode})
-
-    
-    if(!findUrlDetailsByCode){
-    return res.status(404).send({status: false, message : "no such url exist"})
-    }
-
-    res.redirect(findUrlDetailsByCode.longUrl)
-}catch(err){
-    res.status(500).send({error : err.message})
-}
-}
-
-module.exports.urlShortener = urlShortener
-module.exports.getUrl = getUrl
+module.exports.urlShortener = urlShortener;
+module.exports.getUrl = getUrl;
